3b91b049c34ae2b48330f260620687337862d0ec
Nagesh Mathpati <ha3107@gmail.com>
2026-01-12 22:19:35 +0530

    created employer settings page

---
+++ src/features/employers/server/employer-queries.ts
@@ -0,0 +1,34 @@
+
+import { db } from "@/config/db";
+import { employers } from "@/drizzle/schema";
+import { getCurrentUser } from "@/features/auth/server/auth.queries";
+import { eq } from "drizzle-orm";
+
+export const getCurrentEmployerDetails = async () => {
+  const currentUser = await getCurrentUser();
+
+  console.log("currentUser: ", currentUser);
+
+  if (!currentUser) return null;
+
+  if (currentUser.role !== "employer") return null;
+
+  const [employer] = await db
+    .select()
+    .from(employers)
+    .where(eq(employers.id, currentUser.id));
+
+  console.log("employer: ", employer);
+
+  const isProfileCompleted =
+    employer.name &&
+    employer.description &&
+    employer.organizationType &&
+    employer.yearOfEstablishment;
+
+
+  return{ ...currentUser, employerDetails: employer, isProfileCompleted };
+
+}
+
+export default getCurrentEmployerDetails;
